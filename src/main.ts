172f935f9d5221e24917b48fbd5bc4870f153e4e
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-06-24 14:04:39 -0300

    Criação repositorio Tracker-vue

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import roteador from './roteador'
+import { store, key } from './store/index';
+
+createApp(App)
+    .use(roteador)
+    .use(store, key)
+    .mount('#app')
